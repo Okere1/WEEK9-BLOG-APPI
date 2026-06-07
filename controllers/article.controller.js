ecdd4b1cea4cad8b5f7cbfaee32aef0dd1c0a2d3
Promise Okere <Promise.Okere@interswitchng.com>
2026-06-07 19:32:40 +0100

    code update

--- controllers/article.controller.js
+++ controllers/article.controller.js
@@ -22,17 +22,21 @@ const postArticle = async (req, res, next) => {
 };
 
 const getAllArticles = async (req, res, next) => {
-  const { page = 1, limit = 10 } = req.query;
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+
   const skip = (page - 1) * limit;
+
   try {
-    const articles = await (await Article.find())
+    const articles = await Article.find()
       .sort({ createdAt: -1 })
       .skip(skip)
       .limit(limit);
 
-    res
-      .status(200)
-      .json({ message: "Articles retrieved successfully", data: articles });
+    res.status(200).json({
+      message: "Articles retrieved successfully",
+      data: articles,
+    });
   } catch (error) {
     next(error);
   }
