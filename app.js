20dd1da97cd8b095a2f8c422c7b3c70526cfa1ae
Promise Okere <Promise.Okere@interswitchng.com>
2026-06-07 18:56:59 +0100

    code update

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const connectDB = require("./database/connectDB");
+const RequestLogger = require("./middlewares/requestLogger");
+const errorHandler = require("./middlewares/errorHandler");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+connectDB();
 
 app.use(express.json());
 app.use(cors("*"));
-
-connectDB();
+app.use(RequestLogger);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
